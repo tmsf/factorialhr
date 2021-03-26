fd7daf4100d9def145c0a7e9791a99850c7e2612
Tiago Fernandes <dev@tiago.tf>
2021-03-26 18:49:27 +0100

    rename script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mv factorial_settings.json factorial_settings.example.json
 cp factorial_settings.mine.json factorial_settings.json
-python3.9 main_clock_in_till_yesterday.py
+python3.9 main_clock_in_until_yesterday.py
 mv factorial_settings.example.json factorial_settings.json
