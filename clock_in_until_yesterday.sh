f85f4e3f4649c55f004148586ccd2144db8cb9c0
Manuel García <mgarciadelojo@gmail.com>
2021-09-03 13:42:48 +0200

    Get last month and year and use it as a starting point to clock in until yesterday (#2)
    * Add basic editorconfig
    
    * Ignore virtualenv folder
    
    * Get last month and year and use it as a starting point to clock in until yesterday
    
    * Fix virtualenv location
    
    * Fix issue with year

---
+++ clock_in_until_yesterday.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mv factorial_settings.json factorial_settings.example.json
+cp factorial_settings.mine.json factorial_settings.json
+
+source .virtualenv/bin/activate
+python3 main_clock_in_until_yesterday.py
+mv factorial_settings.example.json factorial_settings.json
